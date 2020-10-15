3a36cdaddcb0f7835de687583c965b4b06c9ddde
boredream <48262906@qq.com>
2020-10-15 11:56:55 +0800

    opt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter_todo/data_provider.dart';
-import 'package:flutter_todo/projects_screen.dart';
-import 'package:redux/redux.dart';
 
 import 'pages/DishPage.dart';
 import 'pages/HttpLoad.dart';
@@ -10,14 +6,6 @@ import 'pages/MainPage.dart';
 import 'pages/TodoDetailPage.dart';
 
 void main() {
-//  final store = Store<CategoryState>(stateReducer,
-//      initialState: CategoryState([
-//        Category(0, Icons.cake, Colors.blue, "吃吃吃", [
-//          Task(0, "Task", false),
-//        ]),
-//        Category(1, Icons.content_paste, Colors.orange, "Work", []),
-//      ]));
-//  runApp(FlutterReduxApp(store: store));
   runApp(MyApp());
 }
 
@@ -45,56 +33,6 @@ class MyApp extends StatelessWidget {
           return MainPage();
         });
       },
-    );
-  }
-}
-
-class FlutterReduxApp extends StatelessWidget {
-  final Store<CategoryState> store;
-  const FlutterReduxApp({Key key, this.store}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StoreProvider<CategoryState>(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            textTheme: TextTheme(
-              body1: TextStyle(color: Colors.white, fontSize: 28.0),
-              body2: TextStyle(color: Colors.white54, fontSize: 14.0),
-              display1: TextStyle(color: Colors.black87, fontSize: 36.0),
-              caption: TextStyle(
-                  color: Colors.white,
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold),
-              subhead: TextStyle(color: Colors.black54, fontSize: 12.0),
-            )),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-      store: store,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StoreConnector<CategoryState, Color>(
-          converter: (store) => store.state.categories[0].color,
-          builder: (context, color) => ProjectsScreen(
-                backgroundColor: color,
-              )),
     );
   }
 }
