dbd44d6494455f507c717a7a1b1ac3320c69016c
boredream <48262906@qq.com>
2020-12-17 11:01:58 +0800

    opt

--- lib/pages/AboutPage.dart
+++ lib/pages/AboutPage.dart
@@ -28,13 +28,7 @@ class _PageState extends State<AboutPage> {
             Text(UpdateHelper.appName),
             Text(UpdateHelper.version),
             SizedBox(height: 32),
-            Text(
-                // "由kl和pp共同创建的恋爱app！\n"
-                // "\n"
-                // "本来是我和女朋友自己用的，试用一段时间后觉得还行！就决定优化后上传app供大家使用！\n"
-                // "也希望能收集大家对促进恋爱关系的一些创意想法，好继续完善功能！\n"
-                // "\n"
-                "App现在还在持续更新中，可以通过意见反馈提供您宝贵的意见！"),
+            Text("App现在还在持续更新中，可以通过意见反馈提供您宝贵的意见！\n\n待开发功能：\n * 手机号绑定\n * 日历提醒"),
           ],
         ),
       ),
